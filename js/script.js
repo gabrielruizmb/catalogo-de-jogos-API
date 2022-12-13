f12d09fe46789f0674c796d2ed759ae2ba191499
gabrielruizmb <105463724+gabrielruizmb@users.noreply.github.com>
2022-12-13 13:42:36 -0300

    javascript para exibir lista de jogos

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 // Pega a section do html que irá conter os cards dos jogos.
-let gamesList = document.getElementById('games-list');
-let loadButton = document.getElementById('load-more-button');
-loadButton.addEventListener('click', searchInApi);
-let listCounting = 0;
+let gamesList = document.getElementById('games_list');
 let currentGenre = 'sort-by=popularity';
 let currentPlatform = '&platform=all';
 let currentUrl = currentGenre + currentPlatform;
+let listCounting = 0;
+/*
+let loadButton = document.getElementById('load-more-button');
+loadButton.addEventListener('click', searchInApi);
 
 let pcButton = document.getElementById('pc-button');
 let mmorpgButton = document.getElementById('mmorpg-button');
@@ -23,7 +24,7 @@ function changePlatform(selectedPlatform) {
   currentUrl = currentGenre + currentPlatform;
   searchInApi(currentUrl);
 }
-
+*/
 // Coloca o método "GET" e a chave de acesso a API em uma const.
 const options = {
 	method: 'GET',
@@ -37,14 +38,21 @@ const options = {
 function createCard(data) {
   
   for (let i = listCounting; i < listCounting + 10; i++){
-    let gameCard = document.createElement('article');
+    let gameCard = document.createElement('article')
+    gameCard.className = 'game_card';
     gameCard.innerHTML = `
-        <a href="${data[i].game_url}">
-        <img src="${data[i].thumbnail}">
-        </a>
-        <h1>${data[i].title}</h1>
-        <p>${data[i].genre}</p>
-        <p>${data[i].platform}</p>
+        <div id="content">
+          <img
+            id="thumbnail"
+            src="${data[i].thumbnail}"
+            alt="narutera"
+          />
+        </div>
+        <h1 id="title">${data[i].title}</h1>
+        <div id="game_type">
+          <p id="genre">${data[i].genre}</p>
+          <p id="platform">${data[i].platform}</p>
+        </div>
     `;
     gamesList.appendChild(gameCard);
   }
